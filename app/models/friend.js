fbba0441797b17b14eeed307fe66e4b5985a49f0
Elton Murillo <emurillo510@gmail.com>
2015-05-31 11:41:36 -0700

    Adds comments to model

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,6 +1,18 @@
+// We import the default value from ember-data into the variable DS.
+// Ember-data exports by default namespace (known as DS) that exposes all the 
+// classes and functuins defined in http://emberjs.com/api/data.
+
 import DS from 'ember-data';
 
+// Define the default export for this model, which will be a subclass of DS.model
+// After this class has been defined, we can import this subclass doing: 
+// import Friend fro 'borrower/models/friend'
+
+// We can also use relative imports. So if we were in another model, we could write
+// import Friend from './friend'
 export default DS.Model.extend({
+
+// DS.attr is the standard way to define attribute with Ember-Data.
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
